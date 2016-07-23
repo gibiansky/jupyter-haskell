d81aaa306c3a52b2014e7e066185d6628f60a5b5
Andrew Gibiansky <andrew.gibiansky@gmail.com>
2016-07-23 16:57:37 -0700

    Add more tests

--- python/tests.py
+++ python/tests.py
@@ -10,11 +10,9 @@ import subprocess
 import os
 from contextlib import contextmanager
 
-import warnings
-with warnings.catch_warnings():
-    warnings.filterwarnings("ignore", category=DeprecationWarning)
-    from jupyter_client.kernelspec import KernelSpecManager
-    from jupyter_client import MultiKernelManager
+from jupyter_client.kernelspec import KernelSpecManager
+from jupyter_client import MultiKernelManager
+import jupyter_client
 
 @contextmanager
 def kernel(kernel_name):
@@ -38,6 +36,11 @@ def kernel(kernel_name):
 
 class TestJupyter(unittest.TestCase):
     """Tests for jupyter package."""
+    def test_messaging_protocol_version(self):
+        """Test that the latest jupyter_client has the expected protocol
+        version; this way, when a new version is released, maintainers are
+        automatically notified via failing tests."""
+        self.assertEqual(jupyter_client.protocol_version, "5.0")
 
     def test_kernelspec_installed(self):
         """Test that the jupyter kernelspec installations work, and that
@@ -54,7 +57,7 @@ class TestJupyter(unittest.TestCase):
         kernelspec = manager.get_kernel_spec("calculator")
         self.assertTrue(os.path.isfile(kernelspec.argv[0]))
 
-    def test_can_get_kernel_info(self):
+    def test_basic_kernel_info(self):
         """Test that we can get kernel info from the basic kernel."""
         with kernel("basic") as client:
             # Send the kernel_info_request and get the reply
@@ -74,7 +77,7 @@ class TestJupyter(unittest.TestCase):
             })
             self.assertEqual(reply["parent_header"]["msg_id"], request_uuid)
 
-    def test_can_execute_code(self):
+    def test_basic_exec(self):
         """Test that we can execute code in the basic kernel."""
         with kernel("basic") as client:
             # Send the kernel_info_request and get the reply
@@ -87,7 +90,7 @@ class TestJupyter(unittest.TestCase):
             })
             self.assertEqual(reply["parent_header"]["msg_id"], request_uuid)
 
-    def test_sending_busy_idle_message(self):
+    def test_basic_busy_idle_message(self):
         """Test that during code execution, busy and idle messages are sent."""
         with kernel("basic") as client:
             # Send the kernel_info_request and get the reply
@@ -105,6 +108,41 @@ class TestJupyter(unittest.TestCase):
                 "execution_state": "idle",
             })
             self.assertEqual(reply["parent_header"]["msg_id"], request_uuid)
+
+    def test_calculator_exec_ok(self):
+        """Tests calculator code execution which succeeds."""
+        with kernel("calculator") as client:
+            # Send the kernel_info_request and get the reply
+            client.execute("Compute [] (Lit 1)")
+            replies = [client.get_iopub_msg(timeout=1) for _ in range(3)]
+
+            types = [msg["header"]["msg_type"] for msg in replies]
+            self.assertEqual(types, ["status", "display_data", "status"])
+
+            contents = [msg["content"] for msg in replies]
+            self.assertEqual(contents, [
+                {"execution_state": "busy"},
+                {"data": {"text/plain": "1"}, "metadata": {}},
+                {"execution_state": "idle"},
+            ])
+
+            reply = client.get_shell_msg()
+            self.assertEqual(reply["header"]["msg_type"], "execute_reply")
+            self.assertEqual(reply["content"], {
+                "status": "ok",
+                "execution_count": 1,
+            })
+
+    def test_calculator_exec_err(self):
+        """Tests calculator code execution which fails."""
+        with kernel("calculator") as client:
+            # Send the kernel_info_request and get the reply
+            client.execute("Compute [] (Var 'x')")
+
+            reply = client.get_shell_msg()
+            self.assertEqual(reply["header"]["msg_type"], "execute_reply")
+            self.assertEqual(reply["content"]["status"], "error")
+
 
 if __name__ == '__main__':
     unittest.main(warnings=[])
