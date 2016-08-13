567d33690a6212936960b79db2c7793970387c7b
Andrew Gibiansky <andrew.gibiansky@gmail.com>
2016-08-13 15:43:06 -0700

    Add a test for haddock coverage staying at 100

---
+++ python/ensure_haddock_coverage.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+Tiny utility script to check that coverage statistics output by stack haddock
+are all 100%.
+"""
+import sys
+import re
+
+def main():
+    """Entry point to ensure-haddock-coverage.py."""
+    # Verify that the number of arguments is correct.
+    if len(sys.argv) != 2:
+        print("Usage: ./ensure-haddock-coverage.py $HADDOCK_OUT",
+              file=sys.stderr)
+        sys.exit(1)
+
+    # Read contents of input file.
+    filename = sys.argv[1]
+    with open(filename, "r") as handle:
+        contents = handle.read()
+
+    # Find all coverage statistics.
+    stats = []
+    for line in contents.split("\n"):
+        pat = " ([0-9]*)% \\([ 0-9]* / [ 0-9]*\\) in '([a-zA-Z.0-9]*)'"
+        match = re.search(pat, line)
+        if match is not None:
+            stats.append((int(match.group(1)), match.group(2)))
+
+    insufficient_coverage = False
+    for coverage, name in stats:
+        if coverage != 100:
+            print("Insufficient Haddock Coverage on {}: {}"
+                  .format(name, coverage))
+            insufficient_coverage = True
+
+    if insufficient_coverage:
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
